6fb63906f6602aa721653f43dfc18ce45a9febf4
Mason Campbell <WaffleDefend29@gmail.com>
2018-01-03 20:58:03 -0700

    PhoneDirectory Example Added

---
+++ PhoneDirectory/PhoneDirectory/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PhoneDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dr = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010\n"
+                + "+1-541-984-3012 <P Reed> /PO Box 530; Pollocksville, NC-28573\n :+1-321-512-2222 <Paul Dive> Sequoia Alley PQ-67209\n"
+                + "+1-741-984-3090 <Peter Reedgrave> _Chicago\n :+1-921-333-2222 <Anna Stevens> Haramburu_Street AA-67209\n"
+                + "+1-111-544-8973 <Peter Pan> LA\n +1-921-512-2222 <Wilfrid Stevens> Wild Street AA-67209\n"
+                + "<Peter Gone> LA ?+1-121-544-8974 \n <R Steell> Quora Street AB-47209 +1-481-512-2222\n"
+                + "<Arthur Clarke> San Antonio $+1-121-504-8974 TT-45120\n <Ray Chandler> Teliman Pk. !+1-681-512-2222! AB-47209,\n"
+                + "<Sophia Loren> +1-421-674-8974 Bern TP-46017\n <Peter O'Brien> High Street +1-908-512-2222; CC-47209\n"
+                + "<Anastasia> +48-421-674-8974 Via Quirinal Roma\n <P Salinger> Main Street, +1-098-512-2222, Denver\n"
+                + "<C Powel> *+19-421-674-8974 Chateau des Fosses Strasbourg F-68000\n <Bernard Deltheil> +1-498-512-2222; Mount Av.  Eldorado\n"
+                + "+1-099-500-8000 <Peter Crush> Labrador Bd.\n +1-931-512-4855 <William Saurin> Bison Street CQ-23071\n"
+                + "<P Salinge> Main Street, +1-098-512-2222, Denve\n" + "<P Salinge> Main Street, +1-098-512-2222, Denve\n";
+
+            //Phone(dr, "48-421-674-8974");
+            //Phone(dr, "1-921-512-2222");
+            //Phone(dr, "1-908-512-2222");
+            //Phone(dr, "1-541-754-3010");
+            //Phone(dr, "1-121-504-8974");
+            //Phone(dr, "1-498-512-2222");
+            //Phone(dr, "1-098-512-2222");
+            Console.WriteLine(Phone(dr, "5-555-555-5555"));
+        }
+
+        public static string Phone(string strng, string num)
+        {
+            string returnedContact = "";
+            List<string> contacts = Contacts(strng);
+
+            foreach (string contact in contacts)
+            {
+                if (!contact.Contains(num))
+                {
+                    returnedContact = "Error => Not found: nb";
+                    break;
+                }
+            }
+
+            return returnedContact;
+        }
+
+        public static List<string> Contacts(string contacts)
+        {
+            bool done = false;
+            List<string> contactsList = new List<string>();
+            StringBuilder stringBuilder = new StringBuilder();
+            char c = contacts[0];
+            int counter = 0;
+
+            while (!done)
+            {
+                stringBuilder.Append(c);
+                if (c == '\n')
+                {
+                    contactsList.Add(stringBuilder.ToString());
+                }
+
+                counter++;
+                c = contacts[counter];
+                if (counter == contacts.Length - 1)
+                    done = true; 
+            }
+
+            return contactsList;
+        }
+
+        //private string example()
+        //{
+        //    string pattern = "@\\d+";
+        //    string text = "My favorite number is 8, while yours is 24.";
+
+        //    Regex regex = new Regex(pattern);
+
+        //    return regex.Match(text).ToString();
+        //}
+
+
+    }
+}
