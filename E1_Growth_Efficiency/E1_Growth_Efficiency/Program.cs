3904cf201831fbc41a51427382ffa89a7eacd5c8
MCampbell08 <WaffleDefend29@gmail.com>
2018-01-16 23:43:07 -0700

    Sorter

---
+++ E1_Growth_Efficiency/E1_Growth_Efficiency/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace E1_Growth_Efficiency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 6, 15, 20, 1, 9 };
+
+            int min = array[0];
+            int max = array[0];
+            int average = 0;
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+                if (array[i] > max)
+                    max = array[i];
+
+                average += array[i];
+            }
+            
+            Console.WriteLine("Min: {0}, Max: {1}, Average: {2}", min, max, average / array.Length);
+        }
+    }
+}
---
+++ SortingLibrary/SortingLibrary/Sorter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingLibrary
+{
+    public class Sorter<T>
+    {
+        public static void BubbleSort(IComparable<T>[] inputArray)
+        {
+            bool done = false;
+            int counter = 0;
+            T objectT = inputArray[counter];
+            int choice = inputArray[counter + 1].CompareTo(inputArray[counter]);
+            while (!done)
+            {
+                if (inputArray[counter + 1].CompareTo(inputArray[counter]))
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+        public static void SelectionSort(IComparable<T>[] inputArray)
+        {
+
+        }
+        public static void InsertionSort(IComparable<T>[] inputArray)
+        {
+
+        }
+    }
+}
