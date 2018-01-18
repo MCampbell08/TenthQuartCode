d730a6741047f22e9c86f67dfd4b7db6fbfd077d
MCampbell08 <WaffleDefend29@gmail.com>
2018-01-17 17:50:55 -0700

    Sorter Code Progress

---
+++ SortingLibrary/SorterProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SortingLibrary;
+
+namespace SorterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[] { 0, 45, 7, 4};
+            Sorter<Int32>.BubbleSort(number);
+        }
+    }
+}
--- SortingLibrary/SortingLibrary/Sorter.cs
+++ SortingLibrary/SortingLibrary/Sorter.cs
@@ -4,31 +4,36 @@ using System.Text;
 
 namespace SortingLibrary
 {
-    public class Sorter<T>
+    public class Sorter<T> where T : IComparable
     {
-        public static void BubbleSort(IComparable<T>[] inputArray)
+        public static void BubbleSort(T[] inputArray)
         {
             bool done = false;
-            int counter = 0;
-            T objectT = inputArray[counter];
-            int choice = inputArray[counter + 1].CompareTo(inputArray[counter]);
-            while (!done)
-            {
-                if (inputArray[counter + 1].CompareTo(inputArray[counter]))
+            while (!done) {
+                int counter = 0;
+                T objectT = inputArray[counter];
+                int choice = inputArray[counter + 1].CompareTo(inputArray[counter]);
+                while (counter < inputArray.Length - 1)
                 {
+                    if (inputArray[counter + 1].CompareTo(inputArray[counter]) > 0)
+                    {
+                        T tempObject = inputArray[counter + 1];
+                        inputArray[counter + 1] = inputArray[counter];
+                        inputArray[counter] = tempObject;
 
-                }
-                else
-                {
-
+                    }
+                    else
+                    {
+                        counter++; 
+                    }
                 }
             }
         }
-        public static void SelectionSort(IComparable<T>[] inputArray)
+        public static void SelectionSort(T[] inputArray)
         {
 
         }
-        public static void InsertionSort(IComparable<T>[] inputArray)
+        public static void InsertionSort(T[] inputArray)
         {
 
         }
