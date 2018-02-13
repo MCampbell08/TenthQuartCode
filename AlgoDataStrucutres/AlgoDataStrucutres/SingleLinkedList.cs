07f5550b4ab1d5e8a1212da478fe8fe2947e676d
MCampbell08 <WaffleDefend29@gmail.com>
2018-02-13 13:41:37 -0700

    Progress Part 2

--- AlgoDataStrucutres/AlgoDataStrucutres/SingleLinkedList.cs
+++ AlgoDataStrucutres/AlgoDataStrucutres/SingleLinkedList.cs
@@ -10,8 +10,8 @@ namespace AlgoDataStrucutres
         {
             public Node nextNode = null;
             public object data = null;
-            private int _count = 0;
         }
+        private int _count = 0;
 
         private Node root = null;
         private Node First { get { return root; } }
@@ -36,14 +36,44 @@ namespace AlgoDataStrucutres
                 root = n;
             else
                 Last.nextNode = n;
+            _count++;
         }
         public void Insert(T val, int index)
         {
+            if (index < 0 || index == Count || index > Count)
+                throw new IndexOutOfRangeException();
+
             Node n = new Node { data = val };
             for (int i = 0; i < index; i++)
             {
+                n = n.nextNode;
+            }
+            Node nextNode = n.nextNode;
+            n.nextNode = new Node { data = val };
+            _count++;
+        }
+        public int Count { get { return _count; }}
+        public T Get(int index)
+        {
+            if (index < 0 || index == Count || index > Count)
+                throw new IndexOutOfRangeException();
+            
+            Node node = root;
+            for (int i = 0; i < index; i++)
+            {
+                node = node.nextNode;
+            }
+            return (T)node.data;
+        }
+        public T Remove()
+        {
+            Node node = root;
+
+            for ()
+            {
 
             }
+            return node.data;
         }
     }
 }
