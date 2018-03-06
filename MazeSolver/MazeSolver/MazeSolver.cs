82c00169cb8362917c9a801034ac7501588ea9da
Mason Campbell <WaffleDefend29@gmail.com>
2018-03-06 15:12:20 -0700

    MazeSolver Progress

---
+++ MazeSolver/MazeSolver/MazeSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeSolver
+{
+    public class MazeSolver
+    {
+        public class Node
+        {
+            public string Name { get; set; }
+            public bool StartingPoint { get; set; }
+            public bool FinishingPoint { get; set; }
+        }
+    }
+}
